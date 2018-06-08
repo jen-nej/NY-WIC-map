5c2c11dae809fcb566217b71f3478e9bf8678b75
Nathan Jones <nrjjones@gmail.com>
2018-06-07 21:19:28 -0500

    Reset

--- NY_WIC_Locations.R
+++ NY_WIC_Locations.R
@@ -57,12 +57,6 @@ ggmap(mdata,
       xlab = "Longitude") +
     geom_point(data=nylatlon, aes(x = lon, y = lat), color="blue", size=3, show.legend = FALSE)
 
-# Can you create another with green dots?
-ggmap(mdata,
-      extent = "device",
-      ylab = "Latitude",
-      xlab = "Longitude") +
-    geom_point(data=nylatlon, aes(x = lon, y = lat), color="green", size=2, show.legend = FALSE)
 
 
 
